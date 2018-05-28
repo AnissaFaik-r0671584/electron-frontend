09f0aac7f48afb413507e9bdc1234d03d0c4de67
LAPTOP-KP8I9NG8\Anissa Faik <anissa.faik@student.ucll.be>
2018-05-29 00:08:30 +0200

    Final version exam project

---
+++ rest.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $.ajax({
+        type: "GET",
+        url: "http://193.191.177.8:10578/webshopREST/rest/products",
+        dataType: "json",
+        success: function(json){
+            $('#hier').empty()
+            $(json).each(function(index, product) {
+                $('#hier').append($('<li />').text( product.productId + ': ' +product.name ).attr('class', 'list-group-item'));
+                });
+        },
+        error: function(json){
+            alert("something went wrong");
+        },
+    });
+
+});
